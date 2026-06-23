e36740bea3a529b3e31567da856439d732b4c6e2
david <davidklobo2006@gmail.com>
2026-06-23 23:15:52 +0200

    update

--- include/AudioFile.h
+++ include/AudioFile.h
@@ -1,4 +1,24 @@
 #ifndef PLAYLISTMANAGER_AUDIOFILE_H
 #define PLAYLISTMANAGER_AUDIOFILE_H
 
+#include <string>
+
+class AudioFile {
+private:
+    std::string title;
+    int duration;
+    int typeId;   // 1 = song, 2 = podcast
+
+public:
+    AudioFile(std::string t, int d, int Id);
+
+    std::string getBaseDetails() const;
+
+    std::string getBaseDetails();
+    int getTypeId() const {
+        return typeId;
+    }
+
+};
+
 #endif
